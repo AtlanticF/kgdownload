cfd2f725fd43c21ac5c7cae1a4bb6bfd6f4b6cd3
songmeifeng <553178690@qq.com>
2018-02-26 11:09:42 +0800

    v0.0.1 修改

--- kgDownload.php
+++ kgDownload.php
@@ -6,6 +6,38 @@
  * Time: 20:57
  */
 
+function getIp()
+{
+    $arr_ip_header = array(
+        'HTTP_CDN_SRC_IP',
+        'HTTP_PROXY_CLIENT_IP',
+        'HTTP_WL_PROXY_CLIENT_IP',
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'REMOTE_ADDR',
+    );
+    $client_ip = 'unknown';
+    foreach ($arr_ip_header as $key)
+    {
+        if (!empty($_SERVER[$key]) && strtolower($_SERVER[$key]) != 'unknown')
+        {
+            $client_ip = $_SERVER[$key];
+            break;
+        }
+    }
+    return $client_ip;
+}
+
+$ip = getIp();
+if ($ip != '47.97.177.62') {
+    header('Content-Type:application/json; charset=utf-8');
+    $result['msg'] = 'access deny.';
+    $result['code'] = 202;
+    $result['data'] = null;
+    echo json_encode($result);
+    return false;
+}
+
 if (empty($_GET)) {
     header('Content-Type:application/json; charset=utf-8');
     $result['msg'] = null;
